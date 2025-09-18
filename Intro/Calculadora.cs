73f9fed7159fdfd443a521b31b001f9c17c976a4
Pedro <salazar9_5@hotmail.com>
2025-09-18 08:53:49 -0700

    calculadora simple

--- Intro/Calculadora.cs
+++ Intro/Calculadora.cs
@@ -8,7 +8,19 @@
         }
 
         //Resta
+        public int Resta(int Num1, int Num2)
+        {
+            return Num1 - Num2;
+        }
         //Multi
+        public int Multiplicacion(int Num1, int Num2)
+        {
+            return Num1 * Num2;
+        }
         //Div
+        public double Division(int Num1, int Num2)
+        {
+            return Convert.ToDouble( Num1) / Convert.ToDouble( Num2);
+        }
     }
 }
--- Intro/Program.cs
+++ Intro/Program.cs
@@ -8,6 +8,12 @@ Console.WriteLine("Ingrese el numero 2:");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 int totalsuma = calc.Suma(num1,num2);
+int totalresta = calc.Resta(num1,num2);
+int totalmulti = calc.Multiplicacion(num1,num2);
+double totaldivi = calc.Division(num1,num2);
 
 //Console.WriteLine("El total de la suma es: "+ totalsuma);
 Console.WriteLine($"El total de la suma es: {totalsuma}");
+Console.WriteLine($"El total de la resta es: {totalresta}");
+Console.WriteLine($"El total de la multiplicacion es: {totalmulti}");
+Console.WriteLine($"El total de la division es: {totaldivi}");
