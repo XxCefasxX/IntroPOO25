4bf106b4df11ec023481b7aa2aad61a68cc80c66
Pedro <salazar9_5@hotmail.com>
2025-09-18 08:36:17 -0700

    first commit

---
+++ Intro/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿namespace Intro
+{
+    public class Calculadora
+    {
+        public int Suma(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+
+        //Resta
+        //Multi
+        //Div
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,13 @@
+﻿using Intro;
+
+Calculadora calc = new Calculadora();
+
+Console.WriteLine("Ingrese el numero 1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el numero 2:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int totalsuma = calc.Suma(num1,num2);
+
+//Console.WriteLine("El total de la suma es: "+ totalsuma);
+Console.WriteLine($"El total de la suma es: {totalsuma}");
